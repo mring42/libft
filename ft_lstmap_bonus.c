2fbdd139b5cf5f497d2a5e0b6bf5755935999901
Maximilian Ring <mring@student.42heilbronn.de>
2024-10-24 10:19:32 +0200

    libft complete

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mring <mring@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 16:35:25 by mring             #+#    #+#             */
-/*   Updated: 2024/10/23 16:36:00 by mring            ###   ########.fr       */
+/*   Updated: 2024/10/24 10:10:00 by mring            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,29 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	;
+	void	*new_content;
+	t_list	*new_node;
+	t_list	*new_list;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
+	{
+		new_content = f(lst->content);
+		if (!new_content)
+		{
+			ft_lstclear(&new_list, free);
+			return (NULL);
+		}
+		new_node = ft_lstnew(new_content);
+		if (!new_node)
+		{
+			ft_lstclear(&new_list, free);
+			return (del(new_content), NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
 }
